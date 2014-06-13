deed907790ac1a3045f8a7ee327cb9ff91aa4101
John Keefe <john@johnkeefe.net>
2014-06-12 23:12:40 -0400

    rewrite myInputSketch to toggle all LEDs with button

---
+++ myInputSketch5.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+
+#set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+
+# pin 22, set as input. This is attached to one side of switch, ground is attached to the other.
+# so we engage pull-up resistor (pull-down if to 3.3v instead of ground)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# set pin 23-25 to output for the LEDs
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+def ledToggle(channel):
+
+	# to set output to high:
+	# GPIO.output(12, GPIO.HIGH) or
+	# GPIO.output(12, 1) or
+	# GPIO.output(12, True)
+
+	# to toggle LEDs, get the input and set it as the opposite with 'not'
+	GPIO.output(23, not GPIO.input(23))
+	GPIO.output(24, not GPIO.input(24))
+	GPIO.output(25, not GPIO.input(25))
+
+# GPIO.add_event_detect(22, GPIO.RISING, callback=ledToggle, bouncetime=300)
+
+while True:
+	GPIO.wait_for_edge(22, GPIO.FALLING, callback=ledToggle)
+	print("Button 2 Pressed")
+
+	GPIO.wait_for_edge(22, GPIO.RISING, callback=ledToggle)
+	print("Button 2 Released")
+
+GPIO.cleanup()
+
+# must run this script as sudo to access pins
+
